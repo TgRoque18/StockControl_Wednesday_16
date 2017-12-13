dccc49aff40a24b361c50b8632e1fbf426f71d7a
Fabricio Henrique <fabricio.h93@gmail.com>
2017-12-13 18:24:44 -0200

    classe stock

--- StockControl/StockControl/Class/Stock.cs
+++ StockControl/StockControl/Class/Stock.cs
@@ -6,7 +6,85 @@ using System.Threading.Tasks;
 
 namespace StockControl.Class
 {
-    class Stock
+    public class Stock
     {
+        private int id;
+        private int quantity;
+        private bool active;
+        private Product product;
+        private string name;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        public bool Active
+        {
+            get
+            {
+                return active;
+            }
+
+            set
+            {
+                active = value;
+            }
+        }
+
+        public Product Product
+        {
+            get
+            {
+                return product;
+            }
+
+            set
+            {
+                product = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+    
+        public Stock (int quantity, bool active, Product product, string name)
+        {
+            this.Quantity = quantity;
+            this.Active = active;
+            this.Product = product;
+            this.Name = name;
+        }
     }
 }
