2bb5bf48a7c6ae2c523dcfb9d345dcd7b72b281c
Matheus <matheusjut@gmail.com>
2017-12-13 18:21:30 -0200

    Adição das propriedades das classes Category e Log

--- StockControl/StockControl/Class/Category.cs
+++ StockControl/StockControl/Class/Category.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace StockControl.Class
 {
-    class Category
+   public class Category
     {
+        private int id;
+        private string name;
+        private bool active;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public bool Active
+        {
+            get
+            {
+                return active;
+            }
+
+            set
+            {
+                active = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public Category(string name, bool active) //Construtor
+        {
+            this.Name = name;
+            this.Active = active;
+        }
     }
 }
--- StockControl/StockControl/Class/Log.cs
+++ StockControl/StockControl/Class/Log.cs
@@ -6,7 +6,63 @@ using System.Threading.Tasks;
 
 namespace StockControl.Class
 {
-    class Log
+    public class Log
     {
+        private int id;
+        private string description;
+        private DateTime date;
+        private LogType logType;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return date;
+            }
+
+            set
+            {
+                date = value;
+            }
+        }
+
+        public LogType LogType
+        {
+            get
+            {
+                return logType;
+            }
+
+            set
+            {
+                logType = value;
+            }
+        }
     }
 }
