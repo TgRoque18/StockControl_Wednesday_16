0c12bd9a4adfd246fdbf477e19a076dc5ea44ee8
Pablo Pinheiro Castro <pablo10544@hotmail.com>
2017-12-19 17:12:43 -0200

    Criacao de Solution UserProfileAllForm e  UserProfileDetailsForm

---
+++ StockControl/StockControl/Forms/UserProfileAllForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace StockControl.Forms
+{
+    partial class UserProfileAllForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.tbxSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(36, 57);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(79, 20);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Pesquisar";
+            // 
+            // tbxSearch
+            // 
+            this.tbxSearch.Location = new System.Drawing.Point(148, 57);
+            this.tbxSearch.Name = "tbxSearch";
+            this.tbxSearch.Size = new System.Drawing.Size(159, 26);
+            this.tbxSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Image = global::StockControl.Properties.Resources.Search_icon_old1;
+            this.btnSearch.Location = new System.Drawing.Point(349, 49);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(40, 43);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // UserProfileAllForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 123);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.tbxSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Name = "UserProfileAllForm";
+            this.Text = "UserProfileAllForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox tbxSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
---
+++ StockControl/StockControl/Forms/UserProfileDetails.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace StockControl.Forms
+{
+    partial class UserProfileDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.lblActive = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnErase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Image = global::StockControl.Properties.Resources.SaveIcon1;
+            this.btnSave.Location = new System.Drawing.Point(122, 200);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(51, 32);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Image = global::StockControl.Properties.Resources.BackIcon3;
+            this.btnBack.Location = new System.Drawing.Point(72, 200);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(45, 32);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(41, 50);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(59, 20);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Nome :";
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(122, 50);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(102, 26);
+            this.tbxName.TabIndex = 4;
+            // 
+            // lblActive
+            // 
+            this.lblActive.AutoSize = true;
+            this.lblActive.Location = new System.Drawing.Point(41, 105);
+            this.lblActive.Name = "lblActive";
+            this.lblActive.Size = new System.Drawing.Size(52, 20);
+            this.lblActive.TabIndex = 5;
+            this.lblActive.Text = "Ativo :";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(122, 104);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnErase
+            // 
+            this.btnErase.Image = global::StockControl.Properties.Resources.DeleteIcon;
+            this.btnErase.Location = new System.Drawing.Point(179, 200);
+            this.btnErase.Name = "btnErase";
+            this.btnErase.Size = new System.Drawing.Size(45, 32);
+            this.btnErase.TabIndex = 0;
+            this.btnErase.UseVisualStyleBackColor = true;
+            // 
+            // UserProfileDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblActive);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnErase);
+            this.Name = "UserProfileDetails";
+            this.Text = "UserProfileDetails";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnErase;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Label lblActive;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
