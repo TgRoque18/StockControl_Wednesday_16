1c77bad861a50e7147aacb699fdf193c62c727b7
Pablo Pinheiro Castro <pablo10544@hotmail.com>
2017-12-19 17:23:47 -0200

    Criacao de form Login e Recuperacao de senha

---
+++ StockControl/StockControl/Forms/LoginAllForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace StockControl.Forms
+{
+    partial class LoginAllForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.btnPasswordRecover = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(54, 85);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(64, 20);
+            this.lblPassword.TabIndex = 0;
+            this.lblPassword.Text = "Senha :";
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(54, 47);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(56, 20);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Login :";
+            // 
+            // btnPasswordRecover
+            // 
+            this.btnPasswordRecover.Location = new System.Drawing.Point(143, 149);
+            this.btnPasswordRecover.Name = "btnPasswordRecover";
+            this.btnPasswordRecover.Size = new System.Drawing.Size(108, 52);
+            this.btnPasswordRecover.TabIndex = 2;
+            this.btnPasswordRecover.Text = "Recuperar Senha";
+            this.btnPasswordRecover.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(21, 149);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(92, 52);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Logar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(128, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // LoginAllForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 212);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnPasswordRecover);
+            this.Controls.Add(this.lblLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Name = "LoginAllForm";
+            this.Text = "LoginAllForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Button btnPasswordRecover;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ StockControl/StockControl/Forms/PasswordRecoverAllForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace StockControl.Forms
+{
+    partial class PasswordRecoverAllForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.btnRecoverPassword = new System.Windows.Forms.Button();
+            this.tbxLogin = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Location = new System.Drawing.Point(35, 72);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(56, 20);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Login :";
+            // 
+            // btnRecoverPassword
+            // 
+            this.btnRecoverPassword.Location = new System.Drawing.Point(107, 152);
+            this.btnRecoverPassword.Name = "btnRecoverPassword";
+            this.btnRecoverPassword.Size = new System.Drawing.Size(100, 48);
+            this.btnRecoverPassword.TabIndex = 1;
+            this.btnRecoverPassword.Text = "Recuperar Senha";
+            this.btnRecoverPassword.UseVisualStyleBackColor = true;
+            // 
+            // tbxLogin
+            // 
+            this.tbxLogin.Location = new System.Drawing.Point(125, 72);
+            this.tbxLogin.Name = "tbxLogin";
+            this.tbxLogin.Size = new System.Drawing.Size(100, 26);
+            this.tbxLogin.TabIndex = 2;
+            // 
+            // PasswordRecoverAllForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.tbxLogin);
+            this.Controls.Add(this.btnRecoverPassword);
+            this.Controls.Add(this.lblLogin);
+            this.Name = "PasswordRecoverAllForm";
+            this.Text = "PasswordRecoverAllForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Button btnRecoverPassword;
+        private System.Windows.Forms.TextBox tbxLogin;
+    }
+}
