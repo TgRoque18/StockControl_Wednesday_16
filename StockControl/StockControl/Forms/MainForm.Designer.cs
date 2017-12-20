e6d4a7e8698aaa9d95323d98b1ab192a85d374d6
Fernando <fernandoperigolo@gmail.com>
2017-12-20 17:39:27 -0200

    Mudança para picture box, mudanca no nome e imagens no StockControl

--- StockControl/StockControl/Forms/MainForm.Designer.cs
+++ StockControl/StockControl/Forms/MainForm.Designer.cs
@@ -28,130 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.lblHomeWelcome = new System.Windows.Forms.Label();
-            this.btnUserDDeleteProfile = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.pbxLog = new System.Windows.Forms.PictureBox();
+            this.pbxUserProfile = new System.Windows.Forms.PictureBox();
+            this.pbxCategory = new System.Windows.Forms.PictureBox();
+            this.pbxStock = new System.Windows.Forms.PictureBox();
+            this.pbxProduct = new System.Windows.Forms.PictureBox();
+            this.pbxUser = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLog)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUserProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCategory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxStock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxProduct)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblHomeWelcome
             // 
             this.lblHomeWelcome.AutoSize = true;
             this.lblHomeWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.lblHomeWelcome.Location = new System.Drawing.Point(12, 9);
+            this.lblHomeWelcome.Location = new System.Drawing.Point(9, 7);
+            this.lblHomeWelcome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblHomeWelcome.Name = "lblHomeWelcome";
-            this.lblHomeWelcome.Size = new System.Drawing.Size(144, 20);
+            this.lblHomeWelcome.Size = new System.Drawing.Size(123, 17);
             this.lblHomeWelcome.TabIndex = 1;
             this.lblHomeWelcome.Text = "Seja bem vindo(a)";
-            // 
-            // btnUserDDeleteProfile
-            // 
-            this.btnUserDDeleteProfile.Location = new System.Drawing.Point(51, 62);
-            this.btnUserDDeleteProfile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnUserDDeleteProfile.Name = "btnUserDDeleteProfile";
-            this.btnUserDDeleteProfile.Size = new System.Drawing.Size(95, 95);
-            this.btnUserDDeleteProfile.TabIndex = 14;
-            this.btnUserDDeleteProfile.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(153, 9);
+            this.label1.Location = new System.Drawing.Point(136, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.Size = new System.Drawing.Size(51, 17);
             this.label1.TabIndex = 24;
             this.label1.Text = "Sujeito";
             // 
-            // label2
+            // pbxLog
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label2.Location = new System.Drawing.Point(219, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 20);
-            this.label2.TabIndex = 25;
-            this.label2.Text = "Sair";
+            this.pbxLog.BackgroundImage = global::StockControl.Properties.Resources.Report2_icon_old_;
+            this.pbxLog.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbxLog.Location = new System.Drawing.Point(269, 156);
+            this.pbxLog.Name = "pbxLog";
+            this.pbxLog.Size = new System.Drawing.Size(71, 77);
+            this.pbxLog.TabIndex = 38;
+            this.pbxLog.TabStop = false;
             // 
-            // button1
+            // pbxUserProfile
             // 
-            this.button1.Location = new System.Drawing.Point(51, 203);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 95);
-            this.button1.TabIndex = 26;
-            this.button1.UseVisualStyleBackColor = true;
+            this.pbxUserProfile.BackgroundImage = global::StockControl.Properties.Resources.UserProfileIcon;
+            this.pbxUserProfile.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbxUserProfile.Location = new System.Drawing.Point(158, 156);
+            this.pbxUserProfile.Name = "pbxUserProfile";
+            this.pbxUserProfile.Size = new System.Drawing.Size(71, 77);
+            this.pbxUserProfile.TabIndex = 37;
+            this.pbxUserProfile.TabStop = false;
             // 
-            // button2
+            // pbxCategory
             // 
-            this.button2.Location = new System.Drawing.Point(193, 203);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 95);
-            this.button2.TabIndex = 28;
-            this.button2.UseVisualStyleBackColor = true;
+            this.pbxCategory.BackgroundImage = global::StockControl.Properties.Resources.CategoryStyle_old_1;
+            this.pbxCategory.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbxCategory.Location = new System.Drawing.Point(269, 52);
+            this.pbxCategory.Name = "pbxCategory";
+            this.pbxCategory.Size = new System.Drawing.Size(71, 77);
+            this.pbxCategory.TabIndex = 36;
+            this.pbxCategory.TabStop = false;
             // 
-            // button3
+            // pbxStock
             // 
-            this.button3.Location = new System.Drawing.Point(193, 62);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(95, 95);
-            this.button3.TabIndex = 27;
-            this.button3.UseVisualStyleBackColor = true;
+            this.pbxStock.Location = new System.Drawing.Point(46, 52);
+            this.pbxStock.Name = "pbxStock";
+            this.pbxStock.Size = new System.Drawing.Size(71, 77);
+            this.pbxStock.TabIndex = 35;
+            this.pbxStock.TabStop = false;
             // 
-            // button4
+            // pbxProduct
             // 
-            this.button4.Location = new System.Drawing.Point(334, 203);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 95);
-            this.button4.TabIndex = 30;
-            this.button4.UseVisualStyleBackColor = true;
+            this.pbxProduct.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbxProduct.BackgroundImage")));
+            this.pbxProduct.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbxProduct.Location = new System.Drawing.Point(158, 52);
+            this.pbxProduct.Name = "pbxProduct";
+            this.pbxProduct.Size = new System.Drawing.Size(71, 77);
+            this.pbxProduct.TabIndex = 34;
+            this.pbxProduct.TabStop = false;
             // 
-            // button5
+            // pbxUser
             // 
-            this.button5.Location = new System.Drawing.Point(334, 62);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(95, 95);
-            this.button5.TabIndex = 29;
-            this.button5.UseVisualStyleBackColor = true;
+            this.pbxUser.BackgroundImage = global::StockControl.Properties.Resources.UserProfileIcon;
+            this.pbxUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbxUser.Location = new System.Drawing.Point(46, 156);
+            this.pbxUser.Name = "pbxUser";
+            this.pbxUser.Size = new System.Drawing.Size(71, 77);
+            this.pbxUser.TabIndex = 33;
+            this.pbxUser.TabStop = false;
             // 
-            // button6
+            // pictureBox1
             // 
-            this.button6.Location = new System.Drawing.Point(475, 317);
-            this.button6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(35, 35);
-            this.button6.TabIndex = 31;
-            this.button6.UseVisualStyleBackColor = true;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 248);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.TabIndex = 32;
+            this.pictureBox1.TabStop = false;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 363);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(392, 295);
+            this.Controls.Add(this.pbxLog);
+            this.Controls.Add(this.pbxUserProfile);
+            this.Controls.Add(this.pbxCategory);
+            this.Controls.Add(this.pbxStock);
+            this.Controls.Add(this.pbxProduct);
+            this.Controls.Add(this.pbxUser);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnUserDDeleteProfile);
             this.Controls.Add(this.lblHomeWelcome);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "Controle de estoque";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLog)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUserProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCategory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxStock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxProduct)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,14 +168,13 @@
         #endregion
 
         private System.Windows.Forms.Label lblHomeWelcome;
-        private System.Windows.Forms.Button btnUserDDeleteProfile;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbxUser;
+        private System.Windows.Forms.PictureBox pbxProduct;
+        private System.Windows.Forms.PictureBox pbxStock;
+        private System.Windows.Forms.PictureBox pbxCategory;
+        private System.Windows.Forms.PictureBox pbxUserProfile;
+        private System.Windows.Forms.PictureBox pbxLog;
     }
 }
