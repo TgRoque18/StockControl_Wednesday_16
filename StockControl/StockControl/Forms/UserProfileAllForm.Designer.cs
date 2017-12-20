fdd45a34cd15d8e8b0874c4fe58aa8f355372e58
Fernando <fernandoperigolo@gmail.com>
2017-12-20 17:19:03 -0200

    Correção de perfil de usuario

--- StockControl/StockControl/Forms/UserProfileAllForm.Designer.cs
+++ StockControl/StockControl/Forms/UserProfileAllForm.Designer.cs
@@ -30,46 +30,109 @@
         {
             this.lblSearch = new System.Windows.Forms.Label();
             this.tbxSearch = new System.Windows.Forms.TextBox();
-            this.btnSearch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pbxBack = new System.Windows.Forms.PictureBox();
+            this.pbxEdit = new System.Windows.Forms.PictureBox();
+            this.pbxDelete = new System.Windows.Forms.PictureBox();
+            this.pbxSave = new System.Windows.Forms.PictureBox();
+            this.pbxSearch = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxEdit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSearch)).BeginInit();
             this.SuspendLayout();
             // 
             // lblSearch
             // 
             this.lblSearch.AutoSize = true;
-            this.lblSearch.Location = new System.Drawing.Point(32, 46);
+            this.lblSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearch.Location = new System.Drawing.Point(13, 39);
+            this.lblSearch.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSearch.Name = "lblSearch";
-            this.lblSearch.Size = new System.Drawing.Size(71, 17);
+            this.lblSearch.Size = new System.Drawing.Size(79, 17);
             this.lblSearch.TabIndex = 0;
-            this.lblSearch.Text = "Pesquisar";
+            this.lblSearch.Text = "Pesquisar :";
             // 
             // tbxSearch
             // 
-            this.tbxSearch.Location = new System.Drawing.Point(132, 46);
-            this.tbxSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbxSearch.Location = new System.Drawing.Point(99, 37);
+            this.tbxSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbxSearch.Name = "tbxSearch";
-            this.tbxSearch.Size = new System.Drawing.Size(142, 22);
+            this.tbxSearch.Size = new System.Drawing.Size(108, 20);
             this.tbxSearch.TabIndex = 1;
             // 
-            // btnSearch
+            // dataGridView1
             // 
-            this.btnSearch.Location = new System.Drawing.Point(310, 39);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(36, 34);
-            this.btnSearch.TabIndex = 2;
-            this.btnSearch.UseVisualStyleBackColor = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(70, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(369, 227);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // pbxBack
+            // 
+            this.pbxBack.Location = new System.Drawing.Point(27, 325);
+            this.pbxBack.Name = "pbxBack";
+            this.pbxBack.Size = new System.Drawing.Size(35, 35);
+            this.pbxBack.TabIndex = 4;
+            this.pbxBack.TabStop = false;
+            // 
+            // pbxEdit
+            // 
+            this.pbxEdit.Location = new System.Drawing.Point(413, 325);
+            this.pbxEdit.Name = "pbxEdit";
+            this.pbxEdit.Size = new System.Drawing.Size(35, 35);
+            this.pbxEdit.TabIndex = 5;
+            this.pbxEdit.TabStop = false;
+            // 
+            // pbxDelete
+            // 
+            this.pbxDelete.Location = new System.Drawing.Point(454, 325);
+            this.pbxDelete.Name = "pbxDelete";
+            this.pbxDelete.Size = new System.Drawing.Size(35, 35);
+            this.pbxDelete.TabIndex = 6;
+            this.pbxDelete.TabStop = false;
+            // 
+            // pbxSave
+            // 
+            this.pbxSave.Location = new System.Drawing.Point(372, 325);
+            this.pbxSave.Name = "pbxSave";
+            this.pbxSave.Size = new System.Drawing.Size(35, 35);
+            this.pbxSave.TabIndex = 7;
+            this.pbxSave.TabStop = false;
+            // 
+            // pbxSearch
+            // 
+            this.pbxSearch.Location = new System.Drawing.Point(224, 37);
+            this.pbxSearch.Name = "pbxSearch";
+            this.pbxSearch.Size = new System.Drawing.Size(20, 20);
+            this.pbxSearch.TabIndex = 8;
+            this.pbxSearch.TabStop = false;
             // 
             // UserProfileAllForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(412, 98);
-            this.Controls.Add(this.btnSearch);
+            this.ClientSize = new System.Drawing.Size(524, 372);
+            this.Controls.Add(this.pbxSearch);
+            this.Controls.Add(this.pbxSave);
+            this.Controls.Add(this.pbxDelete);
+            this.Controls.Add(this.pbxEdit);
+            this.Controls.Add(this.pbxBack);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.tbxSearch);
             this.Controls.Add(this.lblSearch);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UserProfileAllForm";
-            this.Text = "UserProfileAllForm";
+            this.Text = "Perfil de Usuário";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxEdit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSearch)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +142,11 @@
 
         private System.Windows.Forms.Label lblSearch;
         private System.Windows.Forms.TextBox tbxSearch;
-        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pbxBack;
+        private System.Windows.Forms.PictureBox pbxEdit;
+        private System.Windows.Forms.PictureBox pbxDelete;
+        private System.Windows.Forms.PictureBox pbxSave;
+        private System.Windows.Forms.PictureBox pbxSearch;
     }
 }
--- StockControl/StockControl/Forms/UserProfileDetails.Designer.cs
+++ StockControl/StockControl/Forms/UserProfileDetails.Designer.cs
@@ -28,37 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSave = new System.Windows.Forms.Button();
-            this.btnBack = new System.Windows.Forms.Button();
             this.lblName = new System.Windows.Forms.Label();
             this.tbxName = new System.Windows.Forms.TextBox();
             this.lblActive = new System.Windows.Forms.Label();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.btnErase = new System.Windows.Forms.Button();
+            this.pbxDelete = new System.Windows.Forms.PictureBox();
+            this.pbxSave = new System.Windows.Forms.PictureBox();
+            this.pbxBack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).BeginInit();
             this.SuspendLayout();
-            // 
-            // btnSave
-            // 
-            this.btnSave.Location = new System.Drawing.Point(108, 160);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(45, 26);
-            this.btnSave.TabIndex = 1;
-            this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // btnBack
-            // 
-            this.btnBack.Location = new System.Drawing.Point(64, 160);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(40, 26);
-            this.btnBack.TabIndex = 2;
-            this.btnBack.UseVisualStyleBackColor = true;
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(36, 40);
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(38, 51);
+            this.lblName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(53, 17);
             this.lblName.TabIndex = 3;
@@ -66,16 +53,19 @@
             // 
             // tbxName
             // 
-            this.tbxName.Location = new System.Drawing.Point(108, 40);
-            this.tbxName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxName.Location = new System.Drawing.Point(98, 51);
+            this.tbxName.Margin = new System.Windows.Forms.Padding(2);
             this.tbxName.Name = "tbxName";
-            this.tbxName.Size = new System.Drawing.Size(91, 22);
+            this.tbxName.Size = new System.Drawing.Size(190, 23);
             this.tbxName.TabIndex = 4;
             // 
             // lblActive
             // 
             this.lblActive.AutoSize = true;
-            this.lblActive.Location = new System.Drawing.Point(36, 84);
+            this.lblActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActive.Location = new System.Drawing.Point(44, 149);
+            this.lblActive.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblActive.Name = "lblActive";
             this.lblActive.Size = new System.Drawing.Size(47, 17);
             this.lblActive.TabIndex = 5;
@@ -84,50 +74,67 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(108, 83);
-            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox1.Location = new System.Drawing.Point(98, 148);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
             this.checkBox1.TabIndex = 6;
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // btnErase
+            // pbxDelete
             // 
-            this.btnErase.Location = new System.Drawing.Point(159, 160);
-            this.btnErase.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnErase.Name = "btnErase";
-            this.btnErase.Size = new System.Drawing.Size(40, 26);
-            this.btnErase.TabIndex = 0;
-            this.btnErase.UseVisualStyleBackColor = true;
+            this.pbxDelete.Location = new System.Drawing.Point(467, 325);
+            this.pbxDelete.Name = "pbxDelete";
+            this.pbxDelete.Size = new System.Drawing.Size(35, 35);
+            this.pbxDelete.TabIndex = 7;
+            this.pbxDelete.TabStop = false;
+            // 
+            // pbxSave
+            // 
+            this.pbxSave.Location = new System.Drawing.Point(426, 325);
+            this.pbxSave.Name = "pbxSave";
+            this.pbxSave.Size = new System.Drawing.Size(35, 35);
+            this.pbxSave.TabIndex = 8;
+            this.pbxSave.TabStop = false;
+            // 
+            // pbxBack
+            // 
+            this.pbxBack.Location = new System.Drawing.Point(31, 325);
+            this.pbxBack.Name = "pbxBack";
+            this.pbxBack.Size = new System.Drawing.Size(35, 35);
+            this.pbxBack.TabIndex = 9;
+            this.pbxBack.TabStop = false;
             // 
             // UserProfileDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(247, 195);
+            this.ClientSize = new System.Drawing.Size(524, 372);
+            this.Controls.Add(this.pbxBack);
+            this.Controls.Add(this.pbxSave);
+            this.Controls.Add(this.pbxDelete);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.lblActive);
             this.Controls.Add(this.tbxName);
             this.Controls.Add(this.lblName);
-            this.Controls.Add(this.btnBack);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.btnErase);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UserProfileDetails";
-            this.Text = "UserProfileDetails";
+            this.Text = "Perfil de Usuário";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnErase;
-        private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.TextBox tbxName;
         private System.Windows.Forms.Label lblActive;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pbxDelete;
+        private System.Windows.Forms.PictureBox pbxSave;
+        private System.Windows.Forms.PictureBox pbxBack;
     }
 }
