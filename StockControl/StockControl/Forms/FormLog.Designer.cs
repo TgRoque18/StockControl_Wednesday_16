292a6a2d4e98df628aa1ac5392f17cf89f18fb3e
Matheus <matheusjut@gmail.com>
2017-12-20 17:22:56 -0200

    mudança forms pra dpi

--- StockControl/StockControl/Forms/FormLog.Designer.cs
+++ StockControl/StockControl/Forms/FormLog.Designer.cs
@@ -53,8 +53,8 @@
             // 
             // FormLog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(523, 364);
             this.Controls.Add(this.btnLogBackPages);
             this.Controls.Add(this.dgvLog);
