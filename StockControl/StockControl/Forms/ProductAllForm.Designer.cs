46cd52f7ce6b5fd5c0260b0780c0eb76e3950f0a
Pablo Pinheiro Castro <pablo10544@hotmail.com>
2018-01-17 15:24:41 -0200

    Criacao productAllForm

---
+++ StockControl/StockControl/Forms/ProductAllForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace StockControl.Forms
+{
+    partial class ProductAllForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.lblactive = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.chbActive = new System.Windows.Forms.CheckBox();
+            this.pbxProductPicture = new System.Windows.Forms.PictureBox();
+            this.pbxSaveImage = new System.Windows.Forms.PictureBox();
+            this.pbxBack = new System.Windows.Forms.PictureBox();
+            this.pbxSave = new System.Windows.Forms.PictureBox();
+            this.pbxDelete = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxProductPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSaveImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblName.Location = new System.Drawing.Point(160, 57);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(64, 25);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Nome";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblPrice.Location = new System.Drawing.Point(160, 109);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(63, 25);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "Preço";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblCategory.Location = new System.Drawing.Point(160, 158);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(97, 25);
+            this.lblCategory.TabIndex = 2;
+            this.lblCategory.Text = "Categoria";
+            // 
+            // lblactive
+            // 
+            this.lblactive.AutoSize = true;
+            this.lblactive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblactive.Location = new System.Drawing.Point(160, 210);
+            this.lblactive.Name = "lblactive";
+            this.lblactive.Size = new System.Drawing.Size(53, 25);
+            this.lblactive.TabIndex = 3;
+            this.lblactive.Text = "ativo";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(263, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 30);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(263, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 30);
+            this.textBox2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(263, 158);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 33);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // chbActive
+            // 
+            this.chbActive.AutoSize = true;
+            this.chbActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.chbActive.Location = new System.Drawing.Point(263, 210);
+            this.chbActive.Name = "chbActive";
+            this.chbActive.Size = new System.Drawing.Size(22, 21);
+            this.chbActive.TabIndex = 9;
+            this.chbActive.UseVisualStyleBackColor = true;
+            // 
+            // pbxProductPicture
+            // 
+            this.pbxProductPicture.Location = new System.Drawing.Point(576, 44);
+            this.pbxProductPicture.Name = "pbxProductPicture";
+            this.pbxProductPicture.Size = new System.Drawing.Size(124, 126);
+            this.pbxProductPicture.TabIndex = 10;
+            this.pbxProductPicture.TabStop = false;
+            // 
+            // pbxSaveImage
+            // 
+            this.pbxSaveImage.Location = new System.Drawing.Point(576, 200);
+            this.pbxSaveImage.Name = "pbxSaveImage";
+            this.pbxSaveImage.Size = new System.Drawing.Size(124, 22);
+            this.pbxSaveImage.TabIndex = 11;
+            this.pbxSaveImage.TabStop = false;
+            // 
+            // pbxBack
+            // 
+            this.pbxBack.Location = new System.Drawing.Point(22, 473);
+            this.pbxBack.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbxBack.Name = "pbxBack";
+            this.pbxBack.Size = new System.Drawing.Size(52, 54);
+            this.pbxBack.TabIndex = 14;
+            this.pbxBack.TabStop = false;
+            // 
+            // pbxSave
+            // 
+            this.pbxSave.Location = new System.Drawing.Point(630, 482);
+            this.pbxSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbxSave.Name = "pbxSave";
+            this.pbxSave.Size = new System.Drawing.Size(52, 54);
+            this.pbxSave.TabIndex = 13;
+            this.pbxSave.TabStop = false;
+            // 
+            // pbxDelete
+            // 
+            this.pbxDelete.Location = new System.Drawing.Point(690, 482);
+            this.pbxDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pbxDelete.Name = "pbxDelete";
+            this.pbxDelete.Size = new System.Drawing.Size(52, 54);
+            this.pbxDelete.TabIndex = 12;
+            this.pbxDelete.TabStop = false;
+            // 
+            // ProductAllForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 572);
+            this.Controls.Add(this.pbxBack);
+            this.Controls.Add(this.pbxSave);
+            this.Controls.Add(this.pbxDelete);
+            this.Controls.Add(this.pbxSaveImage);
+            this.Controls.Add(this.pbxProductPicture);
+            this.Controls.Add(this.chbActive);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblactive);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.lblName);
+            this.Name = "ProductAllForm";
+            this.Text = "ProductAllForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxProductPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSaveImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxSave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxDelete)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label lblactive;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox chbActive;
+        private System.Windows.Forms.PictureBox pbxProductPicture;
+        private System.Windows.Forms.PictureBox pbxSaveImage;
+        private System.Windows.Forms.PictureBox pbxBack;
+        private System.Windows.Forms.PictureBox pbxSave;
+        private System.Windows.Forms.PictureBox pbxDelete;
+    }
+}
