cfab7bdd4049fea332734ea494afb98d1cd3c00e
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-01-17 18:38:30 -0200

    Insert (sql) - UserDetails, Log

--- StockControl/StockControl/Forms/FormLog.cs
+++ StockControl/StockControl/Forms/FormLog.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,10 @@ namespace StockControl.Forms
 {
     public partial class FormLog : Form
     {
+        string connectionString = "workstation id=StockControl.mssql.somee.com;packet size = 4096; user id = luacademy_SQLLogin_1; pwd=msctq6gvt3;data source = StockControl.mssql.somee.com; persist security info=False;initial catalog = StockControl";
+
         public FormLog()
-        {
+        {            
             InitializeComponent();
         }
 
@@ -23,5 +26,35 @@ namespace StockControl.Forms
             mainForm.Show();
             this.Hide();
         }
+
+        public void Save()
+        {
+            SqlConnection sqlConnect = new SqlConnection(connectionString);
+            try
+            {               
+                //Conectar
+                sqlConnect.Open();
+                string sql = "INSERT INTO LOG(NAME, ACTIVE) VALUES (@name, @active)";
+
+                SqlCommand cmd = new SqlCommand(sql, sqlConnect);
+
+                //cmd.Parameters.Add(new SqlParameter("@name", name));
+                //cmd.Parameters.Add(new SqlParameter("@active", active));
+
+                cmd.ExecuteNonQuery();               
+
+            }
+            catch (Exception ex)
+            {
+                //Tratar exceções
+                MessageBox.Show("Erro ao criar log!" + ex.Message);        
+            }
+            finally
+            {
+                //Fechar
+                sqlConnect.Close();
+            }
+        }
+
     }
 }
--- StockControl/StockControl/Forms/UserDetailsForm.cs
+++ StockControl/StockControl/Forms/UserDetailsForm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,9 @@ namespace StockControl
 {
     public partial class UserDetailsForm : Form
     {
+
+        string connectionString = "workstation id=StockControl.mssql.somee.com;packet size = 4096; user id = luacademy_SQLLogin_1; pwd=msctq6gvt3;data source = StockControl.mssql.somee.com; persist security info=False;initial catalog = StockControl";
+
         public UserDetailsForm()
         {
             InitializeComponent();
@@ -27,7 +31,52 @@ namespace StockControl
 
         private void pbxUserSaveProfile_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Usuário salvo com sucesso!");
+            SqlConnection sqlConnect = new SqlConnection(connectionString);
+            try
+            {
+                GetData();
+
+                //Conectar
+                sqlConnect.Open();
+                string sql = "INSERT INTO USER(NAME,PASSWORD,EMAIL,ACTIVE) VALUES (@id, @name, @password, @email, @active)";
+
+                SqlCommand cmd = new SqlCommand(sql, sqlConnect);
+                
+                //cmd.Parameters.Add(new SqlParameter("@name", name));
+                //cmd.Parameters.Add(new SqlParameter("@password", password));
+                //cmd.Parameters.Add(new SqlParameter("@email", email));
+                //cmd.Parameters.Add(new SqlParameter("@active", active));
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Usuário adicionado com sucesso!");
+
+
+            }
+            catch (Exception ex)
+            {
+                //Tratar exceções
+                MessageBox.Show("Erro ao adicionar o usuário!" + ex.Message);
+                CleanData();
+            }
+            finally
+            {
+                //Fechar
+                sqlConnect.Close();
+            }
+        }
+
+
+
+
+        private void CleanData()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void GetData()
+        {
+            throw new NotImplementedException();
         }
     }
 }
