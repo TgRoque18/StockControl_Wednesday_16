f38631a8b94ee0f0c79b87bcc16b502ed0c2a5a8
Pablo <pablo10544@hotmail.com>
2018-01-31 17:39:06 -0200

    Userdetails passando pro banco[ERRO NAO RESOLVIDO]

--- StockControl/StockControl/Forms/UserDetailsForm.cs
+++ StockControl/StockControl/Forms/UserDetailsForm.cs
@@ -1,4 +1,5 @@
-﻿using StockControl.Forms;
+﻿using StockControl.Class;
+using StockControl.Forms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,12 +15,17 @@ namespace StockControl
 {
     public partial class UserDetailsForm : Form
     {
-
+        List<UserProfile> userProfiles = new List<UserProfile>();
         string connectionString = "workstation id=StockControl.mssql.somee.com;packet size = 4096; user id = luacademy_SQLLogin_1; pwd=msctq6gvt3;data source = StockControl.mssql.somee.com; persist security info=False;initial catalog = StockControl";
-
+        public string name;
+        public string password;
+        public string email;
+        public bool active;
         public UserDetailsForm()
         {
             InitializeComponent();
+            LoadComboBox();
+            cmbUserDProfile.DisplayMember = "NAME";
         }
 
         private void pbxUserBackPages_Click(object sender, EventArgs e)
@@ -38,20 +44,20 @@ namespace StockControl
 
                 //Conectar
                 sqlConnect.Open();
-                string sql = "INSERT INTO USER(NAME,PASSWORD,EMAIL,ACTIVE) VALUES (@id, @name, @password, @email, @active)";
+                string sql = "INSERT INTO USER(NAME, PASSWORD, EMAIL, ACTIVE, FK_USER_PROFILE) VALUES (@name, @password, @email, @active, @userProfile)";
 
                 SqlCommand cmd = new SqlCommand(sql, sqlConnect);
-                
-                //cmd.Parameters.Add(new SqlParameter("@name", name));
-                //cmd.Parameters.Add(new SqlParameter("@password", password));
-                //cmd.Parameters.Add(new SqlParameter("@email", email));
-                //cmd.Parameters.Add(new SqlParameter("@active", active));
+
+                cmd.Parameters.Add(new SqlParameter("@name", name));
+                cmd.Parameters.Add(new SqlParameter("@password", password));
+                cmd.Parameters.Add(new SqlParameter("@email", email));
+                cmd.Parameters.Add(new SqlParameter("@active", active));
+                cmd.Parameters.Add(new SqlParameter("@userProfile", cmbUserDProfile.SelectedIndex +1));
 
                 cmd.ExecuteNonQuery();
 
                 MessageBox.Show("Usuário adicionado com sucesso!");
-
-
+                
             }
             catch (Exception ex)
             {
@@ -71,12 +77,54 @@ namespace StockControl
 
         private void CleanData()
         {
-            throw new NotImplementedException();
+            tbxUserDName.Text = "";
+            txbUserDEmail.Text = "";
+            txbUserDPassword.Text = "";
+            txbUserDRepassword.Text = "";
+            ckbUserDActive.Checked = false;
+            cmbUserDProfile.Text = "";
         }
 
         private void GetData()
         {
-            throw new NotImplementedException();
+            name = tbxUserDName.Text;
+            email = txbUserDEmail.Text;
+            password = txbUserDPassword.Text;
+            active = ckbUserDActive.Checked;
+
+        }
+        private void LoadComboBox()
+        {
+
+            SqlConnection cn = new SqlConnection(connectionString);
+
+            try
+            {
+
+                cn.Open();
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM USER_PROFILE", cn);
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    UserProfile up = new UserProfile(Int32.Parse(reader["ID"].ToString()), reader["NAME"].ToString(), bool.Parse(reader["ACTIVE"].ToString()));
+                    //ers.Add(reader["ID"], reader["PRICE"],) // ??? not sure what to put here  as add is not available
+                    userProfiles.Add(up);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar banco. " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            foreach (UserProfile p in userProfiles)
+            {
+                cmbUserDProfile.Items.Add(p);
+            }
+
         }
     }
 }
