8b86376e9d02df53b9612a5a5719bbccd3627d05
Pablo <pablo10544@hotmail.com>
2018-01-31 18:04:53 -0200

    Criacao SELECT no categoryAllForm

--- StockControl/StockControl/Forms/CategoryAllForm.cs
+++ StockControl/StockControl/Forms/CategoryAllForm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,14 @@ using System.Windows.Forms;
 namespace StockControl
 {
     public partial class CategoryAllForm : Form
-    {
+    { string connectionString = "workstation id=StockControl.mssql.somee.com;packet size = 4096; user id = luacademy_SQLLogin_1; pwd=msctq6gvt3;data source = StockControl.mssql.somee.com; persist security info=False;initial catalog = StockControl";
+
         public CategoryAllForm()
         {
             InitializeComponent();
+            ShowData();
+            ResizeDataGridView();
+
         }
 
         private void pbxBack_Click(object sender, EventArgs e)
@@ -37,6 +42,49 @@ namespace StockControl
             CategoryDetailsForm categoryDetailForm = new CategoryDetailsForm();
             categoryDetailForm.Show();
             this.Hide();
+        }
+        private void ShowData()
+        {
+            SqlConnection sqlConnect = new SqlConnection(connectionString);
+
+            try
+            {
+                sqlConnect.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM CATEGORY", sqlConnect);
+                // SqlDataReader reader = cmd.ExecuteReader();
+
+                cmd.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter sqlDtAdapter = new SqlDataAdapter(cmd);
+                sqlDtAdapter.Fill(dt);
+
+                dtgCategory.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao conectar. " + ex.Message);
+            }
+            finally
+            {
+                sqlConnect.Close();
+            }
+        }
+
+        private void ResizeDataGridView()
+        {
+            dtgCategory.Columns["ID"].Visible = false;
+            dtgCategory.Columns["NAME"].HeaderText = "Nome";
+            dtgCategory.Columns["ACTIVE"].HeaderText = "Ativo";
+           
+
+            foreach (DataGridViewColumn col in dtgCategory.Columns)
+            {
+                col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                col.HeaderCell.Style.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Pixel);
+            }
         }
     }
 }
