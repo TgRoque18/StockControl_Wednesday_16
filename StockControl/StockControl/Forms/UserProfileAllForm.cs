f28f5bab582395b4805598e8db21ce5943621de1
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-01-31 18:08:53 -0200

    SELECT USERPROFILE

--- StockControl/StockControl/Forms/UserProfileAllForm.cs
+++ StockControl/StockControl/Forms/UserProfileAllForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,56 @@ namespace StockControl.Forms
 {
     public partial class UserProfileAllForm : Form
     {
+        string connectionString = "workstation id=StockControl.mssql.somee.com;packet size = 4096; user id = luacademy_SQLLogin_1; pwd=msctq6gvt3;data source = StockControl.mssql.somee.com; persist security info=False;initial catalog = StockControl";
+
         public UserProfileAllForm()
         {
             InitializeComponent();
+            ShowData();
+            ResizeDataGridView();
+        }
+
+        private void ShowData()
+        {
+            SqlConnection sqlConnect = new SqlConnection(connectionString);
+
+            try
+            {
+                sqlConnect.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM USER_PROFILE", sqlConnect);
+                // SqlDataReader reader = cmd.ExecuteReader();
+
+                cmd.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter sqlDtAdapter = new SqlDataAdapter(cmd);
+                sqlDtAdapter.Fill(dt);
+
+                dgvUserProfile.DataSource = dt;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao conectar. " + ex.Message);
+            }
+            finally
+            {
+                sqlConnect.Close();
+            }
+        }
+
+        private void ResizeDataGridView()
+        {
+            dgvUserProfile.Columns["ID"].Visible = false;
+            dgvUserProfile.Columns["NAME"].HeaderText = "Nome";
+            dgvUserProfile.Columns["ACTIVE"].HeaderText = "Ativo";
+
+            foreach (DataGridViewColumn col in dgvUserProfile.Columns)
+            {
+                col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+                col.HeaderCell.Style.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Pixel);
+            }
         }
 
         private void pbxBack_Click(object sender, EventArgs e)
