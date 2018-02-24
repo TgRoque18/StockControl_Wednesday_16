c8526a49850a13b96eb0a8ce42c88268e0facc93
Aluno <Aluno@DESKTOP-5FEKJGR>
2018-02-24 10:38:07 -0300

    Alteração no User details (Delete). Design / Code

--- StockControl/StockControl/Forms/UserDetailsForm.cs
+++ StockControl/StockControl/Forms/UserDetailsForm.cs
@@ -292,5 +292,46 @@ namespace StockControl
             }
 
         }
+
+        private void pbxUserDeleteProfile_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(lblId.Text)) //-----
+            {
+                SqlConnection sqlConnect = new SqlConnection(connectionString);
+
+                try
+                {
+                    //Conectar
+                    sqlConnect.Open();
+                    string sql = "DELETE FROM USER_PROFILE WHERE ID = @id";
+
+                    SqlCommand cmd = new SqlCommand(sql, sqlConnect);
+
+                    cmd.Parameters.Add(new SqlParameter("@id", this.lblId.Text));
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Usuário removido com sucesso!");
+
+                    //LogHelper log = new LogHelper();
+                    //log.Insert("User Remove");
+                }
+                catch (Exception ex)
+                {
+                    //Tratar exceções
+                    MessageBox.Show("Erro ao remover usuário!" + "\n\n" + ex.Message);
+                    //throw;
+
+                    //LogHelper logBD = new LogHelper();
+                    //logBD.PrintLog(Convert.ToString(ex));
+                }
+                finally
+                {
+                    //Fechar
+                    sqlConnect.Close();
+                }
+            }
+
+        }
     }
 }
